a1dd9f066a660924cf4c6fd2e6ce31b8f762e6b5
Bora Manga <bora.mng525@gmail.com>
2019-12-04 20:12:52 +0100

    assignment completed

--- app.js
+++ app.js
@@ -9,22 +9,21 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-var foodbring = [
-  { username: "Bilal", food: "Kek" },
-  { username: "Bora", food: "Hiyar" }
-];
+var foodbring = [{ id: 1, name: "Maxim", food: "Paska" }];
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+//Main page
 app.get("/", (req, res) => {
-  res.render("default", { ter: foodbring });
+  res.render("default", { foodbringers: foodbring });
 });
-
+//Adding new items
 app.post("/add", (req, res) => {
-  foodbring.push(req.body);
+  var toAdd = req.body;
+  toAdd["id"] = foodbring.length + 1;
+  foodbring.push(toAdd);
   res.redirect("/");
 });
 
