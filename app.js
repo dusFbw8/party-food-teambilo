2c261bcfe31d47afd666640db90037d8832c5dc5
Bora Manga <bora.mng525@gmail.com>
2019-12-04 16:44:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+var foodbring = [
+  { username: "Bilal", food: "Kek" },
+  { username: "Bora", food: "Hiyar" }
+];
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("default", { ter: foodbring });
+});
+
+app.post("/add", (req, res) => {
+  foodbring.push(req.body);
+  res.redirect("/");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
